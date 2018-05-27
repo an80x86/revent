1b2a62f6734db2039b7ad44bd6056432667b2209
Mustafa Bayer <an80x86@gmail.com>
2018-05-27 23:21:03 +0300

    section 5 end

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import registerServiceWorker from './registerServiceWorker';
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(<App />, rootEl)
+  ReactDOM.render(<App />, rootEl)
 }
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    })
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render)
+  })
 }
 
 render()
